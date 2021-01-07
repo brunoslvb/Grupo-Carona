768ee545a477e2288180cd251b6dcdd016bfd601
Bruno Silva <bruno.silva@edi-labs.com>
2021-01-07 17:17:26 -0300

    Função logout

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoadingController, NavController } from '@ionic/angular';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  loading: any;
+
+  constructor(
+    private nav: NavController,
+    private loadingController: LoadingController,
+    private userService: UserService,
+  ) { }
+
+  async logout(){
+    
+    this.loading = await this.loadingController.create({ spinner: 'crescent' });
+
+    await this.loading.present();
+
+    try {
+      await this.userService.logout();
+      await this.nav.navigateForward('login');
+    } catch (err) {
+      console.error(err);
+    } finally {
+      await this.loading.dismiss();
+    }
+
+  }
 
 }
