29e233a028de3c435739638476f3f3beed7f58f2
Bruno Silva <bruno.silva@edi-labs.com>
2021-01-07 18:00:01 -0300

    Função forgotPassword com envio para o email

--- src/app/pages/forgot/forgot.page.ts
+++ src/app/pages/forgot/forgot.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoadingController, NavController } from '@ionic/angular';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-forgot',
@@ -9,9 +11,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ForgotPage implements OnInit {
 
   forgotForm: FormGroup;
+  loading: any;
 
   constructor(
-    private builder: FormBuilder
+    private nav: NavController,
+    private builder: FormBuilder,
+    private userService: UserService,
+    private loadingController: LoadingController
   ) { }
 
   ngOnInit() {
@@ -20,13 +26,23 @@ export class ForgotPage implements OnInit {
     });
   }
 
-  forgotPassword(){
+  async forgotPassword(){
 
     let domain = (<HTMLInputElement>document.getElementById('domain')).value;
 
     this.forgotForm.value.email += domain;
 
-    console.log(this.forgotForm.value);
+    this.loading = await this.loadingController.create({ spinner: 'crescent' });
+    await this.loading.present();
+
+    try {
+      await this.userService.forgotPassword(this.forgotForm.value.email);
+      await this.nav.navigateForward('login');
+    } catch (err) {
+      console.error(err);
+    } finally {
+      await this.loading.dismiss();
+    }
   }
 
 }
